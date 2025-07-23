f24aff6380913575e2fda8a36b611b5057ff88c2
Szabolcs Toth <54896607+tothszabi@users.noreply.github.com>
2025-07-23 08:27:53 +0200

    First version (#1)

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# bitrise-step-authenticate-with-gcp
+# Authenticate with Google Cloud Platform (GCP)
+
+[![Step changelog](https://shields.io/github/v/release/bitrise-steplib/bitrise-step-authenticate-wth-gcp?include_prereleases&label=changelog&color=blueviolet)](https://github.com/bitrise-steplib/bitrise-step-authenticate-wth-gcp/releases)
+
+The step authenticates with GCP using a service account key.
+
+<details>
+<summary>Description</summary>
+
+This step authenticates with Google Cloud Platform (GCP) using a service account key.
+
+It generates a Google auth token and sets the `GOOGLE_APPLICATION_CREDENTIALS` environment variable.
+
+The step can also log in to Docker to the supplied Artifact Registry locations.
+</details>
+
+## 🧩 Get started
+
+Add this step directly to your workflow in the [Bitrise Workflow Editor](https://devcenter.bitrise.io/steps-and-workflows/steps-and-workflows-index/).
+
+You can also run this step directly with [Bitrise CLI](https://github.com/bitrise-io/bitrise).
+
+## ⚙️ Configuration
+
+<details>
+<summary>Inputs</summary>
+
+| Key | Description | Flags | Default |
+| --- | --- | --- | --- |
+| `service_account_key` | The service account key in JSON format.  You can generate a service account key in the Google Cloud Console. Make sure to grant the necessary permissions to the service account.  The step will use this key to authenticate with GCP and generate a Google auth token. | required, sensitive |  |
+| `docker_login` | Performs Docker login with an auth token.  The step will log in to the Artifact Registry locations specified in the `artifact_registry_locations` input.  It is supported only on the Linux stacks. | required | `false` |
+| `artifact_registry_locations` | A newline (`\n`) separated list of Artifact Registry locations to log in to.  The step will log in to the specified locations using the Google auth token generated from the service account key.  These locations can point ot either the deprecated Container Registry or the new Artifact Registry. |  |  |
+| `verbose` | Enable logging additional information for debugging. | required | `false` |
+</details>
+
+<details>
+<summary>Outputs</summary>
+
+| Environment Variable | Description |
+| --- | --- |
+| `GOOGLE_AUTH_TOKEN` | The generated Google auth token.  This token can be used to authenticate with GCP services. |
+| `GOOGLE_APPLICATION_CREDENTIALS` | The path to the service account key file.  This file is used by GCP SDKs and libraries to authenticate with GCP services. |
+</details>
+
+## 🙋 Contributing
+
+We welcome [pull requests](https://github.com/bitrise-steplib/bitrise-step-authenticate-wth-gcp/pulls) and [issues](https://github.com/bitrise-steplib/bitrise-step-authenticate-wth-gcp/issues) against this repository.
+
+For pull requests, work on your changes in a forked repository and use the Bitrise CLI to [run step tests locally](https://devcenter.bitrise.io/bitrise-cli/run-your-first-build/).
+
+Note: this step's end-to-end tests (defined in e2e/bitrise.yml) are working with secrets which are intentionally not stored in this repo. External contributors won't be able to run those tests. Don't worry, if you open a PR with your contribution, we will help with running tests and make sure that they pass.
+
+Learn more about developing steps:
+
+- [Create your own step](https://devcenter.bitrise.io/contributors/create-your-own-step/)
+- [Testing your Step](https://devcenter.bitrise.io/contributors/testing-and-versioning-your-steps/)
---
+++ step/dependency.go
@@ -0,0 +1,177 @@
+package step
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"runtime"
+	"slices"
+
+	"github.com/bitrise-io/go-utils/v2/command"
+	"github.com/bitrise-io/go-utils/v2/log"
+	"github.com/bitrise-io/go-utils/v2/retryhttp"
+)
+
+const (
+	supportedCLIVersion = "528.0.0"
+	baseURL             = "https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-%s.tar.gz"
+)
+
+func (s *Step) InstallDependencies() error {
+	s.logger.Println()
+	s.logger.Infof("Getting CLI version:")
+
+	version, isInstalled, err := s.installedCLIVersion()
+	if err != nil {
+		return s.installCLI()
+	}
+
+	if isInstalled && version == supportedCLIVersion {
+		s.logger.Printf("CLI version %s is already installed.", version)
+		return nil
+	}
+
+	if !isInstalled {
+		s.logger.Printf("Installing CLI version: %s", version)
+		return s.installCLI()
+	}
+
+	s.logger.Printf("Updating CLI version to: %s", supportedCLIVersion)
+	return s.updateCLI()
+}
+
+func (s *Step) installedCLIVersion() (string, bool, error) {
+	cmd := s.commandFactory.Create("gcloud", []string{"version", "--format", "json", "--quiet"}, nil)
+	output, err := cmd.RunAndReturnTrimmedOutput()
+	if err != nil {
+		return "", false, err
+	}
+
+	var versions map[string]string
+	err = json.Unmarshal([]byte(output), &versions)
+	if err != nil {
+		return "", false, err
+	}
+
+	sdkVersion, ok := versions["Google Cloud SDK"]
+	if !ok {
+		return "", false, err
+	}
+
+	return sdkVersion, true, nil
+}
+
+func (s *Step) installCLI() error {
+	platform, err := getPlatform()
+	if err != nil {
+		return fmt.Errorf("failed to get platform: %w", err)
+	}
+
+	architecture, err := getArchitecture()
+	if err != nil {
+		return fmt.Errorf("failed to get architecture: %w", err)
+	}
+
+	url := fmt.Sprintf(baseURL, fmt.Sprintf("%s-%s-%s", supportedCLIVersion, platform, architecture))
+	tarPath, err := downloadSDK(url, s.logger)
+	if err != nil {
+		return fmt.Errorf("failed to download SDK from %s: %w", url, err)
+	}
+	defer os.Remove(tarPath)
+
+	sdkPath, err := s.extractSDK(tarPath)
+	if err != nil {
+		return fmt.Errorf("failed to extract SDK from %s: %w", sdkPath, err)
+	}
+
+	err = s.setupSDK(sdkPath)
+	if err != nil {
+		return fmt.Errorf("failed to setup SDK: %w", err)
+	}
+
+	return nil
+}
+
+func (s *Step) updateCLI() error {
+	cmd := s.commandFactory.Create("gcloud", []string{"components", "update", fmt.Sprintf("--version=%s", supportedCLIVersion)}, nil)
+	return cmd.Run()
+}
+
+func getPlatform() (string, error) {
+	supportedPlatforms := []string{"darwin", "linux"}
+	if slices.Contains(supportedPlatforms, runtime.GOOS) {
+		return runtime.GOOS, nil
+	}
+
+	return "", fmt.Errorf("unsupported platform: %s", runtime.GOOS)
+}
+
+func getArchitecture() (string, error) {
+	switch runtime.GOARCH {
+	case "amd64":
+		return "x86_64", nil
+	case "arm64":
+		return "arm", nil
+	}
+
+	return "", fmt.Errorf("unsupported architecture: %s", runtime.GOARCH)
+}
+
+func downloadSDK(url string, logger log.Logger) (string, error) {
+	tmpDir, err := os.MkdirTemp("", "")
+	if err != nil {
+		return "", err
+	}
+
+	tarPath := path.Join(tmpDir, "google-cloud-sdk.tar.gz")
+	out, err := os.Create(tarPath)
+	if err != nil {
+		return "", err
+	}
+	defer out.Close()
+
+	resp, err := retryhttp.NewClient(logger).Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	return tarPath, err
+}
+
+func (s *Step) extractSDK(tarPath string) (string, error) {
+	extractedPath := path.Join(path.Dir(tarPath), "gougle-cloud-sdk")
+	if err := os.MkdirAll(extractedPath, os.ModePerm); err != nil {
+		return "", err
+	}
+
+	cmd := s.commandFactory.Create("tar", []string{"-xf", tarPath, "-C", extractedPath}, nil)
+	if output, err := cmd.RunAndReturnTrimmedCombinedOutput(); err != nil {
+		s.logger.Errorf("tar failure: %s", output)
+		return "", err
+	}
+
+	return extractedPath, nil
+}
+
+func (s *Step) setupSDK(path string) error {
+	cmd := s.commandFactory.Create("./install.sh", []string{"--path-update", "true", "--quiet"}, &command.Opts{
+		Dir: path,
+	})
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	cmd = s.commandFactory.Create("./bin/gcloud", []string{"init"}, &command.Opts{
+		Dir: path,
+	})
+	return cmd.Run()
+}
